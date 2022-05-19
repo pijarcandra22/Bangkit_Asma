dd0fdb389f51908d8ae902ffd631d171ce08f275
Ardii122 <85096722+Ardii122@users.noreply.github.com>
2022-05-19 22:35:45 +0700

    tampilan untuk tweet
    tampilan

---
+++ MD/Capstone Project/app/src/main/java/com/udinus/capstoneproject/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.udinus.capstoneproject.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.udinus.capstoneproject.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
